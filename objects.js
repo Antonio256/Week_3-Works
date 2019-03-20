448e6662f3ca687b70fb98226f69cc9dad570307
Anthony Atwine <anthonybakunda@gmail.com>
2019-03-20 13:41:01 +0300

    second  commit

--- objects.js
+++ objects.js
@@ -13,7 +13,17 @@ function Contact (firstName, lastName, phoneNumber,emailAddress, localAddress) {
   this.localAddress = localAddress
 };
 
+var contact1 = new Contact ("Atwine", "Antonio", "256772122083", "anthonybakunda@gmail.com", "firstName");
+var contact1 = new Contact ("Atwine", "Antonio", "256772122083", "anthonybakunda@gmail.com", "lastName");
+var contact1 = new Contact ("Atwine", "Antonio", "256772122083", "anthonybakunda@gmail.com", "phoneNumber");
+var contact1 = new Contact ("Atwine", "Antonio", "256772122083", "anthonybakunda@gmail.com", "emailAddress");
 var contact1 = new Contact ("Atwine", "Antonio", "256772122083", "anthonybakunda@gmail.com", "Kasangati");
 
 
+console.log(contact1.firstName);
+console.log(contact1.lastName);
+console.log(contact1.phoneNumber);
+console.log(contact1.emailAddress);
 console.log(contact1.localAddress);
+
+//Address as an object to store the city, street and COuntry
