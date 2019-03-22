f177b0aa423b721249707f9e04404fb82bd4f72d
Anthony Atwine <anthonybakunda@gmail.com>
2019-03-22 12:54:45 +0300

    add contact

--- objects.js
+++ objects.js
@@ -1,8 +1,8 @@
+
 /*
-Contact that Conatct represents objects from n phone contacts address book.
+Contact represents objects from n phone contacts address book.
 It contains first and second names, phone number, Email address
 And local address
-
 */
 function Contact (firstName, lastName, phoneNumber,emailAddress, localAddress) {
 
@@ -13,7 +13,14 @@ function Contact (firstName, lastName, phoneNumber,emailAddress, localAddress) {
   this.localAddress = localAddress
 };
 
-var contact1 = new Contact ("Atwine", "Antonio", "256772122083", "anthonybakunda@gmail.com", "Kasangati");
+var contact1 = new Contact ("Atwine", "Antonio", "256772122083", "anthonybakunda@gmail.com", ["Kampala", "Kamwokya", "Uganda"]);
 
+//console.log(contact1.localAddress);
 
-console.log(contact1.localAddress);
+//Address as an object to store the city, street and Country
+
+  contact1.address = function (){
+    console.log(this.localAddress[0] + " "+this.localAddress[1]+" "+this.localAddress[2]);
+  }
+console.log(contact1.address());
+console.log(contact1);
