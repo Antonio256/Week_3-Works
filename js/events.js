14915375551316c678279fcb719bee3e78667b62
Anthony Atwine <anthonybakunda@gmail.com>
2019-03-23 13:57:34 +0300

    modified events

--- js/events.js
+++ js/events.js
@@ -1,6 +1,38 @@
 // Function to modify the text content of the paragraph
+/*
 const changeText = () => {
     const p = document.querySelector('p');
 
     p.textContent = "I changed because of an inline event handler.";
+
+the above runs the inline event handler attributes*/
+
+/*
+// Function to modify the text content of the paragraph
+const changeText = () => {
+    const p = document.querySelector('p');
+
+    p.textContent = "I changed because of an event handler property.";
 }
+
+// Add event handler as a property of the button element
+const button = document.querySelector('button');
+button.onclick = changeText;
+
+the above runs for event handler property 
+*/
+
+const p = document.querySelector('p');
+const button = document.querySelector('button');
+
+const changeText = () => {
+    p.textContent = "Will I change?";
+}
+
+const alertText = () => {
+    alert('Will I alert?');
+}
+
+// Events can be overwritten
+button.onclick = changeText;
+button.onclick = alertText;
