213b0a1cb5f0381566e3bf2752fa7406b9a81da2
Anthony Atwine <anthonybakunda@gmail.com>
2019-03-23 14:17:44 +0300

    modified events for event listeners

--- js/events.js
+++ js/events.js
@@ -19,9 +19,10 @@ const changeText = () => {
 const button = document.querySelector('button');
 button.onclick = changeText;
 
-the above runs for event handler property 
+the above runs for event handler property
 */
 
+/*
 const p = document.querySelector('p');
 const button = document.querySelector('button');
 
@@ -36,3 +37,44 @@ const alertText = () => {
 // Events can be overwritten
 button.onclick = changeText;
 button.onclick = alertText;
+
+The above runs for event handle properties and event fires from the browser
+
+*/
+
+// Function to modify the text content of the paragraph
+/*
+const changeText = () => {
+    const p = document.querySelector('p');
+
+    p.textContent = "I changed because of an event listener.";
+}
+
+// Listen for click event
+const button = document.querySelector('button');
+button.addEventListener('click', changeText);
+Te code above is similar to the next but the next function sets multiple event listeers on the same element
+*/
+
+const p = document.querySelector('p');
+const button = document.querySelector('button');
+
+const changeText = () => {
+    p.textContent = "Will I change?";
+}
+
+const alertText = () => {
+    alert('Will I alert?');
+}
+
+// Multiple listeners can be added to the same event and element
+button.addEventListener('click', changeText);
+button.addEventListener('click', alertText);
+
+
+// An anonymous function on an event listener
+/*
+button.addEventListener('click', () => {
+    p.textContent = "Will I change?";
+});
+*/
