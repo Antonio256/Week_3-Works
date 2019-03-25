0bbf618821b706af73a1ad3fcfb944383c0794e8
Anthony Atwine <anthonybakunda@gmail.com>
2019-03-25 11:11:39 +0300

    adding files to colors

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+
 jQuery(document).ready(function() {
   jQuery("h1").click(function() {
     alert("This is a header.");
@@ -6,8 +7,45 @@ jQuery(document).ready(function() {
   jQuery("p").click(function() {
     alert("This is a paragraph.");
   });
-  
+
 jQuery("img").click(function() {
   alert("This is an image.");
 });
+});
+
+
+
+// for colors.HTML
+$(document).ready(function() {
+  $("button#green").click(function() {
+    $("body").addClass("green-background");
+  });
+
+  $("button#yellow").click(function() {
+    $("body").addClass("yellow-background");
+  });
+
+  $("button#red").click(function() {
+    $("body").addClass("red-background");
+  });
+});
+
+
+//updated to display all colours
+
+$(document).ready(function() {
+  $("button#green").click(function() {
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  });
+
+  $("button#yellow").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+  });
+
+  $("button#red").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
+  });
 });
